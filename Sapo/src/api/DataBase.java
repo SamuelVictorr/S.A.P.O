aa3298a547193a3a44bd53af4c55a0eea79fb3b1
Ggeasy1574 <gabrielluck492@gmail.com>
2025-10-30 07:03:09 -0300

    updating verifyCPF (Now it works ever with the ".")

--- Sapo/src/api/DataBase.java
+++ Sapo/src/api/DataBase.java
@@ -77,23 +77,29 @@ public class DataBase {
         int digitoVerificador1;
         int digitoVerificador2;
 
-        for(int i = 0; i < cpf.length(); i ++) {
-            String parteExtraida = cpf.substring(i, (1 + i));
+        //Convert the cpf string with the "." in a version without that
+        String stringModificada = cpf.replace(".", "");
+
+        //Get all the element in the cpf and put them on an array
+        for(int i = 0; i < stringModificada.length(); i ++) {
+            String parteExtraida = stringModificada.substring(i, (1 + i));
             int numCpf = Integer.parseInt(parteExtraida);
             lista[i] = numCpf;
         }
+        System.out.println(Arrays.toString(lista));
+
+        //Calculation to verify the 10th element of the CPF
         int calculo = (lista[0] * 10) + (lista[1] * 9) + (lista[2] * 8) + (lista[3] * 7) + ( 6 * lista[4]) + (5 * lista[5]) + ( 4 * lista[6]) + ( lista[7] * 3)+ (lista[8] * 2);
-        System.out.println(calculo);
         int divisaoVerificador1 = (calculo % 11);
         if(divisaoVerificador1 > -1 && divisaoVerificador1 < 2){
             digitoVerificador1 = 0;
         } else {
             digitoVerificador1 = (11 - divisaoVerificador1);
         }
-        int calculo2 = (lista[0] * 11) + (lista[1] * 10) + (lista[2] * 9) + (lista[3] * 8) + ( 7 * lista[4]) + (6 * lista[5]) + ( 5 * lista[6]) + ( lista[7] * 4)+ (lista[8] * 3) + (digitoVerificador1 * 2);
-        System.out.println(calculo2);
-        int divisaoVerificador2 = (calculo2 % 11);
 
+        //Calculation to verify the 11th element of the CPF
+        int calculo2 = (lista[0] * 11) + (lista[1] * 10) + (lista[2] * 9) + (lista[3] * 8) + ( 7 * lista[4]) + (6 * lista[5]) + ( 5 * lista[6]) + ( lista[7] * 4)+ (lista[8] * 3) + (digitoVerificador1 * 2);
+        int divisaoVerificador2 = (calculo2 % 11);
         if(divisaoVerificador2 > -1 && divisaoVerificador2 < 2){
             digitoVerificador2 = 0;
         } else {
@@ -115,7 +121,7 @@ public class DataBase {
 //            addClient("Gabriel","050.149.073.69","(81) 98369-7190","Sla" );
 //            removeClient("050.149.073.69");
 //            updateClient("050.149.073.69", "(81) 98369-7190");
-            if (verifyCPF("05014907369")){
+            if (verifyCPF("050.149.073.69")){
 //                removeClient("050.149.073.69");
                 System.out.println("It works!!");
             } else {
